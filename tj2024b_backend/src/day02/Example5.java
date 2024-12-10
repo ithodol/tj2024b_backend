325f7e17717986b63790acac2a570febc0375a74
ithodol <ddmdjesz1@gmail.com>
2024-12-10 17:32:26 +0900

    Example5 문제풀이

--- tj2024b_backend/src/day02/Example5.java
+++ tj2024b_backend/src/day02/Example5.java
@@ -1,9 +1,103 @@
 package day02;
 
+import java.util.Scanner;
+
 public class Example5 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
+	      // [지문1] 국어 , 영어 , 수학 점수를 각 입력받아서 각 변수에 저장하고 총점 과 평균을 계산하여 console 출력하시오.
+//	      byte 국어 = scan.nextByte();
+//	      System.out.println("국어 점수 : " + 국어);
+//	      byte 영어 = scan.nextByte();
+//	      System.out.println("영어 점수 : " + 영어);
+//	      byte 수학 = scan.nextByte();
+//	      System.out.println("수학 점수 : " + 수학);
+//	      int 총점 = 국어 + 영어 + 수학;
+//	      System.out.println("총점 : " + 총점);
+//	      int 평균 = 총점 / 3 ;
+//	      System.out.println("평균 : " + 평균);
+//		
+		
+		
+		
+	      // [지문2] 반지름를 입력받아서 원넓이[반지름*반지름*3.14] 계산하여 console 출력하시오.
+//	      float 반지름1 = scan.nextFloat();
+//	      System.out.println("반지름1" + 반지름1);
+//	      float 반지름2 = scan.nextFloat();
+//	      System.out.println("반지름2" + 반지름2);
+//	      double 원넓이 = 반지름1 * 반지름2 * 3.14;
+//	      System.out.println("원의 넓이 : " + 원넓이);
+	      
+		
+		
+		
+	      // [지문3] 두 실수를 입력받아서 앞 실수의 값이 뒤실수의 값의 비율% 계산하여 console 출력하시오.
+//	      float 실수1 = scan.nextFloat();
+//	      System.out.println("실수1 : " + 실수1);
+//	      float 실수2 = scan.nextFloat();
+//	      System.out.println("실수2 : " + 실수2);
+//	      float 비율 = (실수1/실수2) *100;
+//	      System.out.println("비율 : " + 비율);
+ 
+	      
+	      
+		
+		
+	      // [지문4] 정수를 입력받아 입력받은 값이 홀수이면 true / 짝수이면 false 로 console 출력하시오.
+//	      int 정수 = scan.nextInt();
+//	      System.out.println("값1 : " + 정수);
+//	      boolean 결과 = (정수 % 2) == 1;
+//	      System.out.println("결과 : " + 결과);
+		
+		
+		
+		
+	      // [지문5] 정수를 입력받아 입력받은 값이 7의 배수이면 true / 아니면 false 로 console 출력하시오.
+//	      int 정수 = scan.nextInt();
+//	      System.out.println("정수 : " + 정수);
+//	      boolean 결과 = (정수%7) ==0;
+//	      System.out.println("결과 : " + 결과);
+		
+		
+		
+	      // [지문6] 정수를 입력받아 입력받은 값이 홀수 이면서 7배수 이면 true / 아니면 false 로 console 출력하시오.
+//	      int 정수 = scan.nextInt();
+//	      System.out.println("정수 : " + 정수);
+//	      boolean 결과 = (정수%2) ==1 && (정수%7) ==0;
+//	      System.out.println("결과 : " + 결과);
+		
+		
+		
+		
+	      // [지문7] 십만원 단위의 금액을 입력받아 입력받은 금액의 지폐수를 console 출력하시오.
+	          // -> 입력 예] 356789 입력시 , 출력 예] 십만원:3장 만원:5장 천원6장
+//		 int 금액 = scan.nextInt();
+//		 System.out.println("금액 : " + 금액);
+//		 	 
+//		 int 십만원권 = 금액/100000;
+//		 int 만원권 = (금액%100000)/10000;
+//		 int 천원권 = (금액%10000)/1000;
+//		 String 장 = "장";
+//		 System.out.printf("십만원 : "+십만원권+" %s, ",장);
+//		 System.out.printf("만원 : "+만원권+" %s, ",장);
+//		 System.out.printf("천원 : "+천원권+" %s ",장);
+		 
+		
+		
+		
+	      // [지문8] 1차점수 와 2차점수 각각 입력받아서 
+	         // -> 총점이 150점이상이면 '합격' 아니면 '불합격' 으로 console 출력하시오.
+//	      int 점수1 = scan.nextInt();
+//	      System.out.println("점수1 : " + 점수1);
+//	      int 점수2 = scan.nextInt();
+//	      System.out.println("점수2 : " + 점수2);
+//	      int 총점 = 점수1 + 점수2;
+//	      System.out.println("총점 : " + 총점);
+//	      String 합격여부 = 총점 >= 150 ? (String) "합격" : "불합격";
+//	      System.out.println("합격여부 : " + 합격여부);
+
 
 	}
 
