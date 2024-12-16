835b1a22693dfb842d7412c20459a64206940b5a
ithodol <ddmdjesz1@gmail.com>
2024-12-16 15:33:02 +0900

    -

---
+++ tj2024b_backend/src/day06/Example3.java
@@ -0,0 +1,24 @@
+package day06;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		
+		// 1분 복습 : 
+		// 생각해보기 : 생활 속의 객체들을 클래스 코드로 표현하기 (예시 제외)
+		// 게임
+		
+		public class Game{
+			String username;
+			int level;
+			String server;
+			String job;
+		}
+		
+		
+		
+		
+
+	}
+
+}
---
+++ tj2024b_backend/src/day06/Student.java
@@ -0,0 +1,13 @@
+package day06;
+
+public class Student { // class start
+	
+	
+	// 멤버변수 : 객체가 가지는 속성 설계
+	int srudentID; 		// 학번
+	String studentName; // 학생이름
+	int grade;			// 학년
+	String address;		// 사는 곳
+	
+	
+} // class end
