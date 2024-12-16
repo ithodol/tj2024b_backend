4d7af9762e6071f908902bf1154e0246bdcac2c4
ithodol <ddmdjesz1@gmail.com>
2024-12-16 14:27:59 +0900

    ㅇ

--- tj2024b_backend/src/day06/BoardProgram1.java
+++ tj2024b_backend/src/day06/BoardProgram1.java
@@ -1,22 +1,81 @@
 package day06; // 현재 클래스가 위치한 패키지명/폴더명
 
+import java.util.Scanner;
+
 /*
 	실습1 : 게시판 프로그램
+		- 문법 : 변수, 연산자, 제어문(조건문/반복문) 
 		- 제목, 내용, 작성자를 입력받아 저장/출력/삭제/수정
-		- 문법
-			변수, 연산자, 제어문(조건문/반복문) 
-
-
+		- 게시물 3개 저장하기. 게시물 1개당 제목/내용/작성자 => 저장소 3개 필요
+										게시물이 3개 => 저장소 9개 필요 
 */
 
 public class BoardProgram1 { // class start // 자바의 모든 코드는 class안에 존재한다
 
 	public static void main(String[] args) { // main start // 자바의 모든 실행코드는 main 안에서 시작된다
 		
-
-		while(true) { // 무한루프 // 무한반복문
+		// [6-1] 변수, 타입(boolean char byte short int long float double) 참조타입(String, Scanner ...)
+		String 제목1 = null;
+		String 내용1 = null;
+		String 작성자1 = null;
+		
+		String 제목2 = null;
+		String 내용2 = null;
+		String 작성자2 = null;
+		
+		String 제목3 = null;
+		String 내용3 = null;
+		String 작성자3 = null;
+		
+		// null 이란? 참조값이 없다는 뜻. 문자열이 없다는 뜻 
+		// null 값을 넣지 않으면 변수값 호출시 오류 발생함
+		
+		while(true) { // [1] 무한루프/무한반복문 while start
 			
-		}
+			// [2] 출력함수
+			System.out.println("\n\n ===== 게시판 프로그램 =====");
+			System.out.println("1.등록 2.출력 : ");
+			// [3] 입력함수
+				// 1. 입력객체 : Scanner 변수명 = new Scanner(System.in);
+			Scanner scan = new Scanner(System.in);
+				// 2. 입력객체를 이용한 입력함수 : 변수명.nextXXX();
+			int 선택메뉴 = scan.nextInt(); // 우리가 정의한 메뉴번호 선택받기 // 입력받은 값을 저장
+			// [4] 연산자, == 같다
+			// [5] 조건문 if(조건문){ }
+			if(선택메뉴 == 1) {System.out.println(">> 1.등록 선택했습니다.");
+				System.out.println("> 제목 : ");		String title = scan.next();
+				System.out.println("> 내용 : ");		String content = scan.next();  // 메세지(print) -> 입력(next) -> 저장(변수)
+				System.out.println("> 작성자 : ");	String writer = scan.next();
+				// [7] 변수값에 따른 데이터 검사 후 대입 // 첫번재 게시물이 비어있으면 // [6-2] 변수값 수정
+				if(제목1 == null) {제목1 = title; 내용1 = content; 작성자1 = writer;} // while 밖 변수에 입력받은 3개의 문자열을 각각 저장
+				else if(제목2 == null) {제목2 = title; 내용2 = content; 작성자2 = writer;} // 두번째 게시물이 비어있으면
+				else if(제목3 == null) {제목3 = title; 내용3 = content; 작성자3 = writer;} // 세번째 게시물이 비어있으면
+				else {System.out.println("* 등록실패 : 게시물의 작성공간이 부족합니다. *");} // 그 외
+				
+				// 저장은 한 번만 하면 되니까 아래에서는 else if 쓸 필요 없음
+				
+			} // if end
+			else if(선택메뉴 == 2) {System.out.println(">> 2.출력 선택했습니다.");
+				// [7-2] 변수값에 따른 데이터 검사 후 출력 // [6-3] 변수값 호출 // 연결 연산자 : "문자열" + 변수명 + "문자열" + 변수명 + "문자열" + 변수명
+				if(제목1 != null) {System.out.println(">> 제목 : " + 제목1 + ">> 내용 : " + 내용1 + ">> 작성자1 : " + 작성자1);}
+				if(제목2 != null) {System.out.println(">> 제목 : " + 제목2 + ">> 내용 : " + 내용2 + ">> 작성자 : " + 작성자2);}
+				if(제목3 != null) {System.out.println(">> 제목 : " + 제목3 + ">> 내용 : " + 내용3 + ">> 작성자 : " + 작성자3);}
+				// 오류가 발생하는 이유 : 초기값(처음 변수를 만들 때 값을 넣는 행위)이 없기 때문
+				// 초기값이 없다(쓰레기값 - 변수를 유지하기 위한 컴퓨터가 만든 임의값)
+			} // if end
+			
+
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		} // while end
 		
 	} // main end
 
---
+++ tj2024b_backend/src/day06/Example1.java
@@ -0,0 +1,65 @@
+package day06;
+
+import java.util.Scanner;
+
+/*
+	자바 과제3 : 식당 대기번호 발행 프로그램
+		- 문법 : 변수, 연산자, 제어문(조건문/반복문) 
+		- 전화번호, 인원수를 입력받아 저장/출력 구현
+		- 총 대기명단은 3개까지 가능
+			대기명단 1개당 전화번호/인원수 저장소(변수) 2개 필요
+			대기명단3 => 변수 6개 필요
+*/
+
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		// [5] 각 연락처와 인원수를 각 3개를 저정할 변수를 초기화함
+		//		각 사용자에게 입력받은 값들을 저장할 변수를 선언
+		String phone1 = null;	int count1 = 0;
+		String phone2 = null;	int count2 = 0;
+		String phone3 = null;	int count3 = 0;
+		
+		
+		while(true) { // [1] 무한반복문
+			// [2] 출력함수 프로그램의 메뉴 메세지 출력
+			System.out.print("1.대기등록 2.대기현황 : "); // print()함수를 이용하여 사용자에게 등록과 출력 메뉴를 보여준다
+			
+			// [3] 입력함수 Scanner 객체 필요
+			Scanner scan = new Scanner(System.in);
+			int choose = scan.nextInt(); //nextInt() 함수 이용하여 정수타입을 입력받아 정수타입 변수에 저장했다
+			
+			// [4] 조건문 논리 연산에 따른 제어
+			// if(흐름조건), 입력받은 값(메뉴선택)에 따른 흐름제어 (1이면, 2이면)
+			if(choose == 1) {System.out.println(">> 대기명단 등록 <<");
+			
+				// [5] 각 사용자에게 필요한 데이터 입력받기
+				System.out.print("전화번호 : ");		String phone = scan.next();
+				System.out.print("인원수 : ");		int count = scan.nextInt();
+				
+				// [6] 만약 비어있는 명단이 있으면 각 입력받은 데이터를 비어있는 변수에 대입한다. 비어있다. / 문자열 = null, 숫자 = 0으로 판단함
+				if(phone1 == null) {phone1 = phone; count1 = count;}
+				else if(phone2 == null) {phone2 = phone; count2 = count;}
+				else if(phone3 == null) {phone3 = phone; count3 = count;}
+			}else if(choose == 2) {System.out.println(">> 대기명단 현황 <<");}
+				// [7] 만약 비어있지 않으면 각각 변수값 호출
+				if(phone1 != null) {System.out.printf("인원수 %d명, 연락처 : %s \n", count1, phone1);}
+				if(phone2 != null) {System.out.printf("인원수 %d명, 연락처 : %s \n", count2, phone2);}
+				if(phone3 != null) {System.out.printf("인원수 %d명, 연락처 : %s \n", count3, phone3);}
+		}
+
+
+			
+	}
+		
+		
+		
+	
+		
+		
+	}
+		
+		
+
