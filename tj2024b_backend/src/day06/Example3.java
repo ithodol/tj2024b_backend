83e4e08121c27307bee92a32d35d333414f20eba
ithodol <ddmdjesz1@gmail.com>
2024-12-16 15:35:42 +0900

    -

--- tj2024b_backend/src/day06/Example3.java
+++ tj2024b_backend/src/day06/Example3.java
@@ -8,12 +8,12 @@ public class Example3 {
 		// 생각해보기 : 생활 속의 객체들을 클래스 코드로 표현하기 (예시 제외)
 		// 게임
 		
-		public class Game{
-			String username;
-			int level;
-			String server;
-			String job;
-		}
+//		public class Game{
+//			String username;
+//			int level;
+//			String server;
+//			String job;
+//		}
 		
 		
 		
