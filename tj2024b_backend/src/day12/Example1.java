d80ad7741e61a637ddc55a8851572ed3e26d576b
ithodol <ddmdjesz1@gmail.com>
2024-12-24 18:18:06 +0900

    this 예약어

--- tj2024b_backend/src/day12/Example1.java
+++ tj2024b_backend/src/day12/Example1.java
@@ -1,6 +1,7 @@
 package day12;
 
 // - 하나의 .java 파일의 1개 이상의 클래스를 생성할 수 있다.
+// (2) 객체(인스턴스)를 생성하기 위한 객체 설계도 클래스
 class BirthDay{
 	// 클래스 멤버		// *모든 인스턴스들이 각 멤버변수를 사용하는 변수 = 힙메모리
 	// 1. 멤버변수
@@ -10,14 +11,15 @@ class BirthDay{
 	// 3. 메소드/멤버함수 // *모든 인스턴스(객체)들이 공통으로 사용하는 함수 = 정적메모리
 	public void setYear(int year) {this.year = year;} // this 란? 해당 메소드를 호출하는 인스턴스를 가리킨다
 	public void printThis() {System.out.println(this);}
-} // class BrithDay start
+	
+} // class BrithDay end
 
-// (2) 객체(인스턴스)를 생성하기 위한 객체 설계도 클래스
+
 
 // (1) main 함수를 갖는 실행 클래스
 public class Example1 {
 	public static void manin(String[] args) {
-		// 1. 인스턴스 생성 - 객체를 구현하기 위해 설계도(클래스)를 메모리 할당해서 실체물
+		// 1. 인스턴스 생성 - 객체를 구현하기 위한 설계도(클래스)를 메모리 할당해서 실체물로 만듦
 		BirthDay dDay = new BirthDay();
 		// 2. 인스턴스가 메소드를 호출
 		dDay.printThis();
