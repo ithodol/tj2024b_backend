577ec522233f15d991f00a920100975cb1ee0838
ithodol <ddmdjesz1@gmail.com>
2025-01-13 15:38:06 +0900

    기본클래스

--- tj2024b_backend/src/day24/StringExample.java
+++ tj2024b_backend/src/day24/StringExample.java
@@ -1,5 +1,7 @@
 package day24;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class StringExample {
@@ -64,18 +66,19 @@ public class StringExample {
 		System.out.println(gender); // 1
 		
 			// [활용] .nextChar() 가 없는 이유 : .next().charAt(0) : 한글자 입력받기 가능
-		Scanner scan = new Scanner(System.in);
-		char ch = scan.next().charAt(0); // 입력받은 문자열의 첫글자 추출
+		//Scanner scan = new Scanner(System.in);
+		//char ch = scan.next().charAt(0); // 입력받은 문자열의 첫글자 추출
 		
 			// [6] .length()
 		System.out.println("012345-1230123".length()); // 14
 			// [활용] 반복문 : 변수명.length()-1 => 마지막인덱스 
 		//for(int index = 0; index <= 변수명.length()-1; index++) {}
 		
-			// [7] .replace(기존문자열, 새로우문자열)
+			// [7] .replace(기존문자열, 새로운문자열)
 		String str8 = "자바프로그래밍".replace("자바", "JAVA");
 		System.out.println(str8); // JAVA프로그래밍
 			// [활용] html<br/> / java(\n)
+		//html에서 사용하는 줄바꿈 <br/>이 존재하면 JAVA에서 사용하는 줄바꿈 \n으로 치환하는 방법
 		String htmlData = "유재석<br/>안녕하세요.";
 		System.out.println(htmlData); // 유재석<br/>안녕하세요
 		String javaData = htmlData.replace("<br/>", "\n");
@@ -83,10 +86,49 @@ public class StringExample {
 		// 유재석
 		// 안녕하세요
 		
-		// html에서 사용하는 줄바꿈 <br/>이 존재하면 JAVA에서 사용하는 줄바꿈 \n으로 치환하는 방법
+			// [8] .subString(시작인덱스, [끝인덱스])
+			// 첫번째 인덱스부터 6번인덱스 전까지 추출
+		String str9 = "012345-1230123".substring(0,6);
+		System.out.println(str9); // 012345
+			// 7번 인덱스부터 (끝인덱스생략)마지막인덱스까지 추출
+		String str10 = "012345-1230123".substring(7); // 끝인덱스 생략
+		System.out.println(str10);	// 1230123
+			// [9] .split("구분문자")
+		String[] str11 = "012345-1230123".split("-");
+		System.out.println(str11[0]);	// 012345
+		System.out.println(str11[1]); 	// 1230123
+			// [활용] CSV 파일 다루기
 		
-	
+			// [10] .indexOf("찾을문자열")
+		int findIndex = "자바 프로그래밍 언어".indexOf("자바");
+		System.out.println(findIndex); 	// 0
 		
+			// [11] .contains("찾을문자열")
+		boolean findBool = "자바 프로그래밍 언어".contains("자바");
+		System.out.println(findBool); // true
+		
+			// [12] .getBytes()
+		byte[] str12 = "JAVA PROGRAM".getBytes();
+		System.out.println(str12);
+		System.out.println(Arrays.toString(str12));
+		// [74, 65, 86, 65, 32, 80, 82, 79, 71, 82, 65, 77]
+			// 영문/일부특수문자 : 문자 1개당 1바이트(아스키코드)
+		System.out.println( "ab".getBytes().length ); // 2
+		// - 한글 : 문자1개당 2바이트( 유니코드 ) + 1바이트(utf-8규칙정보) => 3바이트
+		System.out.println( "가나".getBytes().length ); // 6
+			// char 2바이트 표현  short +-32,000개 --> short unsigned 65,536개
+			// --> char 유니코드로 된 문자 1개 표현 
+			// [활용] : 인증코드 만들기
+		String code = "";
+		for( int i = 1 ; i<=6 ; i++ ) {
+			System.out.println( new Random() ); // 랜덤(난수) 관련 클래스 
+			System.out.println( new Random().nextInt() ); 		// int타입의 난수 반환함수
+			System.out.println( new Random().nextInt(26) );		// 0 ~ 25 사이의 난수 반환 함수.
+			System.out.println( new Random().nextInt(26) + 97 ); // 97 ~ 122 사이의 난수 반환 함수.
+			System.out.println( (char)(new Random().nextInt(26) + 97) );
+			code += (char)( new Random().nextInt(26) + 97 );
+		} // f end 
+		System.out.println( code ); // 임의의 난수 문자열 생성 
 		
 	} // main end
 	
